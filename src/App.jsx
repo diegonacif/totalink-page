03f8317b71170782a91cc6cc84d6e36ef6513d0c
Diego Nacif <diegoreisnacif@gmail.com>
2022-10-22 11:50:53 -0300

    feat: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Customers } from './components/Customers/Customers';
+import { Footer } from './components/Footer/Footer';
 import { Header } from './components/Header/Header';
 import { Modal } from './components/Modal/Modal';
 import { Trust } from './components/Trust/Trust';
@@ -12,6 +13,7 @@ function App() {
       <Modal />
       <Trust />
       <Customers />
+      <Footer />
     </main>
     )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="address">
+        <h4>Endereço:</h4>
+        <span>
+          Rua Asp. Santos, 100<br />
+          Centro, Parnamirim/RN<br />
+          CEP 59140-295<br />
+        </span>
+        <h4>Fones:</h4>
+        <span>
+          (84) 2020-5444 / 
+          (84) 3272-2149
+        </span>
+      </div>
+      <div className="copyrights">
+        <span>© 2022 Total Ink - Todos os direitos reservados.</span>
+      </div>
+    </div>
+  );
+};
