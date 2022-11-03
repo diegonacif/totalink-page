98f859134aa76b1d998f92662a88eeed1cdb8c82
diegonacif <diegoreisnacif@gmail.com>
2022-11-03 07:50:35 -0300

    feat: outside click hook

--- src/components/MenuButton/MenuButton.jsx
+++ src/components/MenuButton/MenuButton.jsx
@@ -6,6 +6,7 @@ import { Link } from 'react-router-dom';
 
 // Outside Click (Hook)
 import { useOnClickOutside } from 'usehooks-ts'
+import { useTransition, animated } from 'react-spring';
 
 export const MenuButton = () => {
 
@@ -23,6 +24,13 @@ export const MenuButton = () => {
 
   useOnClickOutside(refContainer, handleClickOutside)
 
+  // Modal Animation
+  const transition = useTransition(isMenuOpen, {
+    from: {opacity: 0},
+    enter: {opacity: 1},
+    leave: {opacity: 0},
+  });
+  
   return (
     <div 
       className="menu-button-container"
@@ -34,13 +42,10 @@ export const MenuButton = () => {
         <X size={32} weight="bold" onClick={() => handleMenuOpen()} /> :
         <List size={32} weight="bold" onClick={() => handleMenuOpen()} />
       }
-      {
+      {/* {
         isMenuOpen
         ?
-        <div 
-          className="menu-modal"
-          
-        >
+        <div className="menu-modal">
           <div className="menu-item">
             <Link to="/">Home</Link>
           </div>
@@ -54,7 +59,27 @@ export const MenuButton = () => {
         </div>
         :
         null
+      } */}
+      <div className="test">
+      {
+        transition((style, item) => {
+          item ? 
+          <animated.div className="menu-modal" style={style}>
+            <div className="menu-item">
+              <Link to="/">Home</Link>
+            </div>
+            <hr />
+            <div className="menu-item">
+              <Link to="/quote">Solicitar Orçamento</Link>
+            </div>
+            <div className="menu-item">
+              <Link to="/faq">FAQ</Link>
+            </div>
+          </animated.div> :
+          '';
+        })
       }
+      </div>
       
     </div>
   )
