d3de6d97dbb80083ddec84a41f8755072258a73a
diegonacif <diegoreisnacif@gmail.com>
2022-11-07 23:24:38 -0300

    style: faq category page

--- src/pages/FAQ/FaqToner/FaqToner.jsx
+++ src/pages/FAQ/FaqToner/FaqToner.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { Header } from "../../../components/Header/Header"
 
 import '../../../css/App.css';
+import lessThan from '../../../assets/greater-than-sign.svg';
 
 export const FaqToner = () => {
   return (
@@ -11,10 +12,29 @@ export const FaqToner = () => {
       <header>
         <nav>
           <Link to="/faq">Início</Link>
-          <CaretDoubleRight size={20} color="#241f31" weight="duotone" />
+          <img src={lessThan} alt="" />
         </nav>
         <h1>Problemas com Toner</h1>
       </header>
+      <section>
+        <Link to="#" className="question-row">
+          <span>Quantas páginas meu toner imprime?</span>
+          <img src={lessThan} alt="" />
+        </Link>
+        <Link to="#" className="question-row">
+          <span>Impressões apresentando falhas</span>
+          <img src={lessThan} alt="" />
+        </Link>
+        <Link to="#" className="question-row">
+          <span>Impressões com manchas</span>
+          <img src={lessThan} alt="" />
+        </Link>
+        <Link to="#" className="question-row">
+          <span>Impressões muito claras</span>
+          <img src={lessThan} alt="" />
+        </Link>
+      </section>
+      
     </div>
   )
 }
