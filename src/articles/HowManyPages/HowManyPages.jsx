d49f33185ba09cb7e23023e982b117e42bade102
Diego Nacif <diegoreisnacif@gmail.com>
2022-12-13 12:07:06 -0300

    how many pages content

--- src/articles/HowManyPages/HowManyPages.jsx
+++ src/articles/HowManyPages/HowManyPages.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header/Header';
 import { CopyrightFooter } from '../../components/CopyrightFooter/CopyrightFooter';
 import { ArticleHeader } from '../../components/ArticleHeader/ArticleHeader';
 
+import coberturaImg from '../../assets/faq-assets/area-de-cobertura.png';
 import '../../css/App.css';
 
 export const HowManyPages = () => {
@@ -14,17 +15,10 @@ export const HowManyPages = () => {
       />
       <div className="article-main">
         <div className="article-content">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita corrupti deserunt saepe corporis facilis qui doloremque ea ipsam eos! Ducimus tempora harum iste doloremque aspernatur saepe incidunt tenetur soluta nihil.</p>
-          <p>Quod ducimus suscipit autem cumque sequi nobis consequatur ipsam excepturi neque, minus atque magni adipisci non totam numquam, vitae voluptatibus facere asperiores repellat corrupti voluptatum in voluptates. Id, at distinctio!</p>
-          <p>Aperiam laudantium minus iure assumenda dolorem? Ipsam perferendis architecto veritatis eaque doloremque similique, maxime reprehenderit tenetur a blanditiis tempora laborum eligendi nemo nobis repudiandae et quia, atque rem possimus voluptates?</p>
-          <p>Deleniti fuga officiis quo, delectus labore culpa earum reiciendis sed ex repellendus repudiandae officia dolores incidunt, dicta soluta dignissimos nostrum at deserunt vero ratione hic odio odit ipsa. Doloremque, quaerat?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed libero beatae reiciendis. Rerum facilis in vero consectetur, eligendi sapiente perspiciatis omnis animi quibusdam quis voluptatem, repudiandae deleniti dolorum perferendis quaerat.</p>
-          <p>Cupiditate aliquid provident et enim? Dolores quos enim, voluptatem molestias facilis excepturi perferendis. Dolore inventore non iste magnam atque nam doloribus odit! Ullam inventore odio recusandae quisquam nam, qui assumenda?</p>
-          <p>Optio fugit magnam deleniti tenetur ea excepturi sunt maxime! Sed autem quia blanditiis, doloremque temporibus assumenda! Repellat labore aut modi optio minima exercitationem assumenda fuga libero fugit laborum. Temporibus, eveniet!</p>
-          <p>Sed ea eveniet quis numquam incidunt voluptatem officia odio eum tenetur architecto vel rerum illo a esse fuga doloremque dignissimos quibusdam magnam, iusto distinctio enim nam voluptates? Molestias, odio voluptates.</p>
-          <p>Sed ea eveniet quis numquam incidunt voluptatem officia odio eum tenetur architecto vel rerum illo a esse fuga doloremque dignissimos quibusdam magnam, iusto distinctio enim nam voluptates? Molestias, odio voluptates.</p>
-          <p>Sed ea eveniet quis numquam incidunt voluptatem officia odio eum tenetur architecto vel rerum illo a esse fuga doloremque dignissimos quibusdam magnam, iusto distinctio enim nam voluptates? Molestias, odio voluptates.</p>
-          <p>Sed ea eveniet quis numquam incidunt voluptatem officia odio eum tenetur architecto vel rerum illo a esse fuga doloremque dignissimos quibusdam magnam, iusto distinctio enim nam voluptates? Molestias, odio voluptates.</p>
+          <p>Os fabricantes de impressora informam o número de páginas que um toner imprime tomando como referência a área de cobertura de 5%. Essa área de cobertura normalmente é ultrapassada no cotidiano das impressões.</p>
+          <img src={coberturaImg} alt="Área de cobertura" />
+          <h3>Área de cobertura</h3>
+          <p>A área de cobertura de impressão é o percentual da área da página que está recebendo tinta ou toner durante a impressão. O preenchimento de uma folha em sua área total equivale a cobertura de página impressa, levando em conta o uso de toner ou tinta utilizada nas impressões.</p>
         </div>
       </div>
       <CopyrightFooter />
