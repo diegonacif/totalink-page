f98abf6dc06f3da10ed13b043dab76c4e75338bb
Diego Nacif <diegoreisnacif@gmail.com>
2022-12-19 17:10:32 -0300

    feat: react-reveal added

--- src/components/Customers/Customers.jsx
+++ src/components/Customers/Customers.jsx
@@ -6,37 +6,39 @@ import sterbomLogo from '../../assets/customer-logos/sterbom.png';
 import unilarLogo from '../../assets/customer-logos/unilar.png';
 import natalcorLogo from '../../assets/customer-logos/natalcor.png';
 
+import Fade from 'react-reveal/Fade';
+
 export const Customers = () => {
   return (
     <div className="customers-container">
-      {/* <hr /> */}
-      <h1>Conheça alguns de nossos clientes:</h1>
+      <Fade left cascade duration={800}>
+        <h1>Conheça alguns de nossos clientes:</h1>
 
-      <div className="customers-wrapper">
-        <img src={artCLogo} alt="ArtC logo" id="artCLogo" />
-        <span>Art&C</span>
-      </div>
-      <div className="customers-wrapper">
-        <img src={interfortLogo} alt="Interfort logo" id="interfortLogo" />
-        <span>Interfort</span>
-      </div>
-      <div className="customers-wrapper">
-        <img src={mareLogo} alt="ArtC logo" id="mareLogo" />
-        <span>Maré Mansa</span>
-      </div>
-      <div className="customers-wrapper">
-        <img src={sterbomLogo} alt="ArtC logo" id="sterbomLogo" />
-        <span>Sterbom</span>
-      </div>
-      <div className="customers-wrapper">
-        <img src={unilarLogo} alt="ArtC logo" id="unilarLogo" />
-        <span>Unilar</span>
-      </div>
-      <div className="customers-wrapper">
-        <img src={natalcorLogo} alt="ArtC logo" id="natalcorLogo" />
-        <span>Natalcor</span>
-      </div>
-      
+        <div className="customers-wrapper">
+          <img src={artCLogo} alt="ArtC logo" id="artCLogo" />
+          <span>Art&C</span>
+        </div>
+        <div className="customers-wrapper">
+          <img src={interfortLogo} alt="Interfort logo" id="interfortLogo" />
+          <span>Interfort</span>
+        </div>
+        <div className="customers-wrapper">
+          <img src={mareLogo} alt="ArtC logo" id="mareLogo" />
+          <span>Maré Mansa</span>
+        </div>
+        <div className="customers-wrapper">
+          <img src={sterbomLogo} alt="ArtC logo" id="sterbomLogo" />
+          <span>Sterbom</span>
+        </div>
+        <div className="customers-wrapper">
+          <img src={unilarLogo} alt="ArtC logo" id="unilarLogo" />
+          <span>Unilar</span>
+        </div>
+        <div className="customers-wrapper">
+          <img src={natalcorLogo} alt="ArtC logo" id="natalcorLogo" />
+          <span>Natalcor</span>
+        </div>
+      </Fade>
     </div>
   );
 };
--- src/components/Trust/Trust.jsx
+++ src/components/Trust/Trust.jsx
@@ -1,20 +1,22 @@
 import '../../css/App.css';
+import Fade from 'react-reveal/Fade';
 
 export const Trust = () => {
   return (
     <div className="trust-container">
-      <section className="resume-text">
-        <h1>Uma palavra que nos resume: Confiabilidade</h1>
-        <p>Atuamos no mercado desde 2004. Nos tornamos referência em qualidade na remanufatura de cartuchos para impressoras jato de tinta e laser. A Total Ink dispõe de equipamentos com tecnologia de ponta alemã. A única no estado com corpo de funcionários capacitados e em frequente atualização.<br /><br />
-        Oferecemos uma ampla linha de soluções em impressão que vai desde a remanufatura de cartuchos jato de tinta e toners, até a completa terceirização do seu parque de impressão (outsourcing). Tudo isso com a mais alta qualidade e agilidade com o objetivo de proporcionar completa satisfação do cliente.</p>
-      </section>
-      <section className="feedbacks-wrapper">
-        <div className="feedback">
-          <p>"Quando precisamos o atendimento foi imediato e o nosso problema resolvido através do comodato."</p>
-          <span>Samuel Gondim - SGS Soluções</span>
-        </div>
-       
-      </section>
+      <Fade left cascade duration={800}>
+        <section className="resume-text">
+          <h1>Uma palavra que nos resume: Confiabilidade</h1>
+          <p>Atuamos no mercado desde 2004. Nos tornamos referência em qualidade na remanufatura de cartuchos para impressoras jato de tinta e laser. A Total Ink dispõe de equipamentos com tecnologia de ponta alemã. A única no estado com corpo de funcionários capacitados e em frequente atualização.<br /><br />
+          Oferecemos uma ampla linha de soluções em impressão que vai desde a remanufatura de cartuchos jato de tinta e toners, até a completa terceirização do seu parque de impressão (outsourcing). Tudo isso com a mais alta qualidade e agilidade com o objetivo de proporcionar completa satisfação do cliente.</p>
+        </section>
+        <section className="feedbacks-wrapper">
+          <div className="feedback">
+            <p>"Quando precisamos o atendimento foi imediato e o nosso problema resolvido através do comodato."</p>
+            <span>Samuel Gondim - SGS Soluções</span>
+          </div>
+        </section>
+      </Fade>
     </div>
   )
 }
