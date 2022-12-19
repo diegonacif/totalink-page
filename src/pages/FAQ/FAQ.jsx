df58ae545b0bad251d5b42f8f57f3d6407348d78
Diego Nacif <diegoreisnacif@gmail.com>
2022-12-19 17:21:49 -0300

    feat: react-reveal added

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { Header } from '../../components/Header/Header';
 import { CopyrightFooter } from '../../components/CopyrightFooter/CopyrightFooter';
 
+import Fade from 'react-reveal/Fade';
+
 import '../../css/App.css';
 
 export const FAQ = () => {
@@ -13,24 +15,26 @@ export const FAQ = () => {
           <h1>Encontre respostas rápidas pro seu problema, nas categorias abaixo.</h1>
         </header>
         <div className="faq-categories">
-          <Link to="/faq/toner">
+          <Fade left cascade duration={800}>
+            <Link to="/faq/toner">
+              <div className="faq-category">
+                <span >Toner</span>
+                <div className="faq-category-img toner-card"></div>
+              </div>
+            </Link>
             <div className="faq-category">
-              <span >Toner</span>
-              <div className="faq-category-img toner-card"></div>
+              <span id="drum-card-text">Fotocondutor</span>
+              <div className="faq-category-img drum-card"></div>
             </div>
-          </Link>
-          <div className="faq-category">
-            <span id="drum-card-text">Fotocondutor</span>
-            <div className="faq-category-img drum-card"></div>
-          </div>
-          <div className="faq-category">
-            <span>Erros / Defeitos</span>
-            <div className="faq-category-img error-card"></div>
-          </div>
-          <div className="faq-category">
-            <span>Tanques de Tinta / Bulk</span>
-            <div className="faq-category-img bulk-card"></div>
-          </div>
+            <div className="faq-category">
+              <span>Erros / Defeitos</span>
+              <div className="faq-category-img error-card"></div>
+            </div>
+            <div className="faq-category">
+              <span>Tanques de Tinta / Bulk</span>
+              <div className="faq-category-img bulk-card"></div>
+            </div>
+          </Fade>
         </div>
         <CopyrightFooter />
       </div>
