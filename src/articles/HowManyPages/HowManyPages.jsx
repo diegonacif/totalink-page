ab1f8d2d851a4658ea5ddf0ab172c2a0f133a342
Diego Nacif <diegoreisnacif@gmail.com>
2022-12-22 17:00:35 -0300

    style: responsivity adjustments

--- src/articles/HowManyPages/HowManyPages.jsx
+++ src/articles/HowManyPages/HowManyPages.jsx
@@ -7,21 +7,23 @@ import '../../css/App.css';
 
 export const HowManyPages = () => {
   return (
-    <div className="article-container">
-      <Header />
-      <ArticleHeader
-        navText="Problemas com Toner"
-        titleText="Quantas páginas meu toner imprime?"
-      />
-      <div className="article-main">
-        <div className="article-content">
-          <p>Os fabricantes de impressora informam o número de páginas que um toner imprime tomando como referência a área de cobertura de 5%. Essa área de cobertura normalmente é ultrapassada no cotidiano das impressões.</p>
-          <img src={coberturaImg} alt="Área de cobertura" />
-          <h3>Área de cobertura</h3>
-          <p>A área de cobertura de impressão é o percentual da área da página que está recebendo tinta ou toner durante a impressão. O preenchimento de uma folha em sua área total equivale a cobertura de página impressa, levando em conta o uso de toner ou tinta utilizada nas impressões.</p>
+    <>
+      <div className="article-container">
+        <Header />
+        <ArticleHeader
+          navText="Problemas com Toner"
+          titleText="Quantas páginas meu toner imprime?"
+        />
+        <div className="article-main">
+          <div className="article-content">
+            <p>Os fabricantes de impressora informam o número de páginas que um toner imprime tomando como referência a área de cobertura de 5%. Essa área de cobertura normalmente é ultrapassada no cotidiano das impressões.</p>
+            <img src={coberturaImg} alt="Área de cobertura" />
+            <h3>Área de cobertura</h3>
+            <p>A área de cobertura de impressão é o percentual da área da página que está recebendo tinta ou toner durante a impressão. O preenchimento de uma folha em sua área total equivale a cobertura de página impressa, levando em conta o uso de toner ou tinta utilizada nas impressões.</p>
+          </div>
         </div>
+        <CopyrightFooter />
       </div>
-      <CopyrightFooter />
-    </div>
+    </>
   )
 }
