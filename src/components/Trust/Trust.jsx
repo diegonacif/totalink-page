7ff6e201e8dbe1a9f04c1fc5eea7bf8ce45e30bf
Diego Nacif <diegoreisnacif@gmail.com>
2022-12-26 16:47:22 -0300

    fix: text review

--- src/components/Trust/Trust.jsx
+++ src/components/Trust/Trust.jsx
@@ -7,8 +7,8 @@ export const Trust = () => {
       <Fade left cascade>
         <section className="resume-text">
           <h1>Uma palavra que nos resume: Confiabilidade</h1>
-          <p>Atuamos no mercado desde 2004. Nos tornamos referência em qualidade na remanufatura de cartuchos para impressoras jato de tinta e laser. A Total Ink dispõe de equipamentos com tecnologia de ponta alemã. A única no estado com corpo de funcionários capacitados e em frequente atualização.<br /><br />
-          Oferecemos uma ampla linha de soluções em impressão que vai desde a remanufatura de cartuchos jato de tinta e toners, até a completa terceirização do seu parque de impressão (outsourcing). Tudo isso com a mais alta qualidade e agilidade com o objetivo de proporcionar completa satisfação do cliente.</p>
+          <p>Atuamos no mercado desde 2004. Nos tornamos referência em qualidade na remanufatura de cartuchos para impressoras jato de tinta e laser. A Total Ink dispõe de equipamentos com tecnologia alemã de ponta. A única no estado com corpo de funcionários capacitados e em frequente atualização.<br /><br />
+          Oferecemos uma ampla linha de soluções em impressão que vai desde a remanufatura de cartuchos jato de tinta e toners, até a completa terceirização do seu parque de impressão (outsourcing). Tudo isso com a mais alta qualidade e agilidade, com o objetivo de proporcionar completa satisfação do cliente.</p>
         </section>
         {/* <section className="feedbacks-wrapper">
           <div className="feedback">
