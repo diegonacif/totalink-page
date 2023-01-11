e9775631340747c1845fa397215689d4c400994f
Diego Nacif <diegoreisnacif@gmail.com>
2023-01-11 11:22:10 -0300

    feat: chat 'pedido de toners' conditional rendering

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -99,142 +99,144 @@ export const Chat = () => {
 
   return (
     <div className="chat-container">
-      <div className="header">
-        <UserCircle size={56} color="#ffffff" weight="duotone" />
-        <span className="">Fale conosco</span>
-      </div>
-      <main>
-        <div className="chat-row question">
-          <div className="question-avatar">
-            <img src={totalLogo} alt="Total Ink logo" />
+      <div className="chat-bg">
+        <div className="header">
+          <UserCircle size={56} color="#ffffff" weight="duotone" />
+          <span className="">Fale conosco</span>
+        </div>
+        <main>
+          <div className="chat-row question">
+            <div className="question-avatar">
+              <img src={totalLogo} alt="Total Ink logo" />
+            </div>
+            <span>Com qual setor você deseja falar?</span>
           </div>
-          <span>Com qual setor você deseja falar?</span>
-        </div>
 
-        <div className="chat-row answer">
-          <select 
-            name="department" 
-            id="department"  
-            {...register("department")}
-          >
-            <option value="" disabled>Selecione uma opção</option>
-            <option value="Pedido de toner">Pedido de toners</option>
-            <option value="Problemas técnicos">Problemas técnicos</option>
-            <option value="Setor financeiro">Setor financeiro</option>
-            <option value="Locação de impressoras">Locação de impressoras</option>
-          </select>
-          <UserCircle size={38} color="#ffffff" weight="duotone" />
-        </div>
+          <div className="chat-row answer">
+            <select 
+              name="department" 
+              id="department"  
+              {...register("department")}
+            >
+              <option value="" disabled>Selecione uma opção</option>
+              <option value="Pedido de toner">Pedido de toners</option>
+              <option value="Problemas técnicos">Problemas técnicos</option>
+              <option value="Setor financeiro">Setor financeiro</option>
+              <option value="Locação de impressoras">Locação de impressoras</option>
+            </select>
+            <UserCircle size={38} color="#ffffff" weight="duotone" />
+          </div>
 
-        {/* Condition Toner */}
-        {
-          getValues("department") === "Pedido de toner" ?
-          <>
-            <div className="chat-row question">
-              <div className="question-avatar">
-                <img src={totalLogo} alt="Total Ink logo" />
+          {/* Condition Toner */}
+          {
+            getValues("department") === "Pedido de toner" ?
+            <>
+              <div className="chat-row question">
+                <div className="question-avatar">
+                  <img src={totalLogo} alt="Total Ink logo" />
+                </div>
+                <span>Precisamos saber, por favor, qual o modelo do toner ou impressora.</span>
               </div>
-              <span>Precisamos saber, por favor, qual o modelo do toner ou impressora.</span>
-            </div>
 
-            <div className="chat-row answer">
-              <input 
-                id="subject"
-                label="Modelo do toner ou da impressora" 
-                required
-                aria-invalid={errors.subject ? "true" : "false"} 
-                {...register("model")}
-              />
-              <UserCircle size={38} color="#ffffff" weight="duotone" />
-            </div>
+              <div className="chat-row answer">
+                <input 
+                  id="subject"
+                  label="Modelo do toner ou da impressora" 
+                  required
+                  aria-invalid={errors.subject ? "true" : "false"} 
+                  {...register("model")}
+                />
+                <UserCircle size={38} color="#ffffff" weight="duotone" />
+              </div>
 
-            { 
-              watch("model").length >= 3 ?
-              <>
-                <div className="chat-row question">
-                  <div className="question-avatar">
-                    <img src={totalLogo} alt="Total Ink logo" />
+              { 
+                watch("model").length >= 3 ?
+                <>
+                  <div className="chat-row question">
+                    <div className="question-avatar">
+                      <img src={totalLogo} alt="Total Ink logo" />
+                    </div>
+                    <span>Agora nos diga quantos você está precisando.</span>
                   </div>
-                  <span>Agora nos diga quantos você está precisando.</span>
-                </div>
 
-                <div className="chat-row answer">
-                  <input 
-                    id="subject"
-                    label="Quantidade" 
-                    required
-                    aria-invalid={errors.subject ? "true" : "false"} 
-                    {...register("quantity")}
-                  />
-                  <UserCircle size={38} color="#ffffff" weight="duotone" />
-                </div>
+                  <div className="chat-row answer">
+                    <input 
+                      id="subject"
+                      label="Quantidade" 
+                      required
+                      aria-invalid={errors.subject ? "true" : "false"} 
+                      {...register("quantity")}
+                    />
+                    <UserCircle size={38} color="#ffffff" weight="duotone" />
+                  </div>
 
-                {
-                  watch("quantity").length > 0 ?
-                  <button className="btn-continue">Continuar</button> :
-                  null
-                }
-              </> :
-              null
-            }
-          </> :
-          undefined
-        }
+                  {
+                    watch("quantity").length > 0 ?
+                    <button className="btn-continue">Continuar</button> :
+                    null
+                  }
+                </> :
+                null
+              }
+            </> :
+            undefined
+          }
 
-        {/* Condition Technical problems */}
-        {
-          getValues("department") === "Problemas técnicos" ?
-          <>
+          {/* Condition Technical problems */}
+          {
+            getValues("department") === "Problemas técnicos" ?
+            <>
+              <div className="chat-row answer">
+                <input 
+                  id="subject"
+                  label="Modelo da impressora" 
+                  required
+                  color="success"
+                  variant="standard"
+                  aria-invalid={errors.subject ? "true" : "false"} 
+                  {...register("model")}
+                />
+                <UserCircle size={38} color="#ffffff" weight="duotone" />
+              </div>
+              <div className="chat-row answer">
+                <input 
+                  id="subject"
+                  label="Informe o problema apresentado" 
+                  required
+                  color="success"
+                  variant="standard"
+                  aria-invalid={errors.subject ? "true" : "false"} 
+                  {...register("subject")}
+                />
+                <UserCircle size={38} color="#ffffff" weight="duotone" />
+              </div>
+            </> :
+            undefined
+          }
+
+          {/* Condition Financial */}
+          {
+            getValues("department") === "Setor financeiro" ?
             <div className="chat-row answer">
               <input 
                 id="subject"
-                label="Modelo da impressora" 
+                label="Assunto"
                 required
-                color="success"
-                variant="standard"
-                aria-invalid={errors.subject ? "true" : "false"} 
-                {...register("model")}
-              />
-              <UserCircle size={38} color="#ffffff" weight="duotone" />
-            </div>
-            <div className="chat-row answer">
-              <input 
-                id="subject"
-                label="Informe o problema apresentado" 
-                required
+                multiline
+                maxRows={4}
                 color="success"
                 variant="standard"
                 aria-invalid={errors.subject ? "true" : "false"} 
                 {...register("subject")}
               />
               <UserCircle size={38} color="#ffffff" weight="duotone" />
-            </div>
-          </> :
-          undefined
-        }
-
-        {/* Condition Financial */}
-        {
-          getValues("department") === "Setor financeiro" ?
-          <div className="chat-row answer">
-            <input 
-              id="subject"
-              label="Assunto"
-              required
-              multiline
-              maxRows={4}
-              color="success"
-              variant="standard"
-              aria-invalid={errors.subject ? "true" : "false"} 
-              {...register("subject")}
-            />
-            <UserCircle size={38} color="#ffffff" weight="duotone" />
-          </div> :
-          undefined
-        }
-      </main>
-      <div className="footer">
-        <input type="text" placeholder="Insira sua mensagem" />
+            </div> :
+            undefined
+          }
+        </main>
+        {/* <div className="footer">
+          <input type="text" placeholder="Insira sua mensagem" />
+        </div> */}
       </div>
     </div>
   )
