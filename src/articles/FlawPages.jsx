34d967a1004a29d6cd137caa0606279ba42c4fe5
Diego Nacif <diegoreisnacif@gmail.com>
2023-01-27 16:43:09 -0300

    fix: article content responsivity

--- src/articles/FlawPages.jsx
+++ src/articles/FlawPages.jsx
@@ -21,44 +21,47 @@ export const FlawPages = () => {
       />
       <div className="article-main">
         <div className="article-content">
-          <img src={falhasImg} alt="página com manchas" loading="lazy" />
-          <div className="section-toggle" onClick={() => setLaserShow(!laserShow)}>
-            <span>Impressora à laser</span>
-            {
-              laserShow ?
-              <CaretDown size={16} weight="duotone" /> :
-              <CaretRight size={16} weight="duotone" />
-            }
-          </div>
-          {
-            laserShow ?
-            <>
-              <p>Se áreas na impressão aparecem em branco, ou muito claras, seu toner pode estar acabando. Se possuir algum toner reserva, substitua para verificar se o problema persiste.</p>
-              <p>No caso de impressoras multi funcionais, verifique também se o problema ocorre tanto em impressões diretamente do computador, quanto em cópias.</p>
-              <p>Caso não possua toner reserva, entre em contato com a nossa equipe clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
-            </> :
-            null
-          }
+          <div className="article-row">
+            <img src={falhasImg} alt="página com manchas" loading="lazy" />
+            <div className="article-column">
+              <div className="section-toggle" onClick={() => setLaserShow(!laserShow)}>
+                <span>Impressora à laser</span>
+                {
+                  laserShow ?
+                  <CaretDown size={16} weight="duotone" /> :
+                  <CaretRight size={16} weight="duotone" />
+                }
+              </div>
+              {
+                laserShow ?
+                <>
+                  <p>Se áreas na impressão aparecem em branco, ou muito claras, seu toner pode estar acabando. Se possuir algum toner reserva, substitua para verificar se o problema persiste.</p>
+                  <p>No caso de impressoras multi funcionais, verifique também se o problema ocorre tanto em impressões diretamente do computador, quanto em cópias.</p>
+                  <p>Caso não possua toner reserva, entre em contato com a nossa equipe clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
+                </> :
+                null
+              }
 
-          <div className="section-toggle" onClick={() => setInkShow(!inkShow)}>
-            <span >Impressora Jato de Tinta / Bulk</span>
-            {
-              inkShow ?
-              <CaretDown size={16} weight="duotone" /> :
-              <CaretRight size={16} weight="duotone" />
-            }
+              <div className="section-toggle" onClick={() => setInkShow(!inkShow)}>
+                <span >Impressora Jato de Tinta / Bulk</span>
+                {
+                  inkShow ?
+                  <CaretDown size={16} weight="duotone" /> :
+                  <CaretRight size={16} weight="duotone" />
+                }
+              </div>
+              {
+                inkShow ?
+                <>
+                  <p>Falhas na impressão podem ser devido a falta de tinta no cabeçote de impressão, entupimento nos dutos que transportam a tinta do bulk até o cabeçote, ou em casos mais graves, furos ou outros danos na lâmina do cabeçote.</p>
+                  <p>O procedimento mais básico é realizar uma limpeza no cabeçote. É um processo automatizado que pode ser feito no painel da impressora, ou em um computador que esteja conectado à ela.</p>
+                  <YoutubeEmbed embedId="6I7gmly_kn4" />
+                  <p>Se mesmo após 3 tentativas de limpeza do cabeçote, as impressões continuem com falhas, entre em contato com o suporte técnico clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
+                </> :
+                null
+              }
+            </div>
           </div>
-          {
-            inkShow ?
-            <>
-              <p>Falhas na impressão podem ser devido a falta de tinta no cabeçote de impressão, entupimento nos dutos que transportam a tinta do bulk até o cabeçote, ou em casos mais graves, furos ou outros danos na lâmina do cabeçote.</p>
-              <p>O procedimento mais básico é realizar uma limpeza no cabeçote. É um processo automatizado que pode ser feito no painel da impressora, ou em um computador que esteja conectado à ela.</p>
-              <YoutubeEmbed embedId="6I7gmly_kn4" />
-              <p>Se mesmo após 3 tentativas de limpeza do cabeçote, as impressões continuem com falhas, entre em contato com o suporte técnico clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
-            </> :
-            null
-          }
-
         </div>
       </div>
       <CopyrightFooter />
--- src/articles/StainPages.jsx
+++ src/articles/StainPages.jsx
@@ -18,10 +18,14 @@ export const StainPages = () => {
       />
       <div className="article-main">
         <div className="article-content">
-          <p>Caso sua impressão esteja apresentando manchas escuras que cobrem toda a área impressa, pontos aleatórios ou duplicando a impressão criando imagens “fantasmas”, seu toner pode ser a causa do problema.</p>
-          <img src={manchasImg} alt="página com manchas" loading="lazy" />
-          <p>Se possuir algum toner reserva, substitua para verificar se o problema persiste.</p>
-          <p>Caso não possua, entre em contato com a nossa equipe clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
+          <div className="article-row">
+            <img src={manchasImg} alt="página com manchas" loading="lazy" />
+            <div className="article-column">
+              <p>Caso sua impressão esteja apresentando manchas escuras que cobrem toda a área impressa, pontos aleatórios ou duplicando a impressão criando imagens “fantasmas”, seu toner pode ser a causa do problema.</p>
+              <p>Se possuir algum toner reserva, substitua para verificar se o problema persiste.</p>
+              <p>Caso não possua, entre em contato com a nossa equipe clicando <Link to="/contact" target="_blank">aqui</Link>.</p>
+            </div>
+          </div>
         </div>
       </div>
       <CopyrightFooter />
